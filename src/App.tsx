128f5ef3cbb2df2e7446a77fff3539ac8ba0905d
Max <84030024+max3243f6@users.noreply.github.com>
2026-04-18 17:51:03 +0200

    Change header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
-import './App.css'
+import "./App.css";
 import RepoViewer from './RepoViewer'
+import { Header } from "./Header"
+
 
 function App() {
   const [username, setUsername] = useState("ThiloK1410")
 
 
   return (
-    <div className='main-body'>
-      <div className='header'>
-        THIS IS THE HEADER
-      </div>
+    <div className="main-body">
+      <Header setUsername={setUsername} />
       <div className='git-body'>
         <div className='git-sidebar content-window'>
-          username here
+          {username}
         </div>
         <div className='repo-container content-window'>
           <RepoViewer username={username} />
+          {username}
         </div>
       </div>
     </div>
